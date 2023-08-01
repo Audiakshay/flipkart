1de57ac365abcbd7bd28797ce92dc781dbb6bdba
mubinahvr <81406547+mubinahvr@users.noreply.github.com>
2023-08-01 14:09:36 +0530

    carosel

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import CardSlider from "../Components/CardSlider";
+import Carousal from "../Components/Carousal";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const Home = () => {
   const pro = useSelector((state) => state?.products);
   return (
     <>
+    
+      {/* <Carousal pro={pro?.slider} card="slider" /> */}
+      <Carousal pro={pro?.slider} card="slider" />
       <CardSlider pro={pro?.electronics} card="Electronics" />
       <CardSlider pro={pro?.HomeKitchens} card="Home and Kitchens" />
       <CardSlider pro={pro?.BooksToys} card="Books and Toys" />
