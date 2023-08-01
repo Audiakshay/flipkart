7bb560e9de0a8fd52aba7905be137aa6a07e9673
Sachin <sachinh10171999@gmail.com>
2023-08-01 14:58:06 +0530

    Appliances_Db

---
+++ src/Screens/Appliances.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Appliances() {
+  return (
+    <div>
+      <div>
+        <p>Appliances</p>
+      </div>
+    </div>
+  );
+}
+
+export default Appliances;
