14aac8f34fbfab0261883ad3327dbfa915eb1e4e
Meghamala KM <meghamala.km@logituit.com>
2023-08-02 12:04:42 +0530

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import productsAction from "./Redux/Actions/ProductsList";
+import React from "react";
 import Home from "./Screens/Home";
-import Category from "./Components/Category";
-import Footer from "./Components/Footer";
-import Navbar from "./Components/Navbar";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
-import mobileAction from "./Redux/Actions/MobileAction";
 import MobilePage from "./Screens/MobilePage";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import DashBoard from "./Layout/DashBoard";
